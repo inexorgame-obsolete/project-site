6f4edea8b26300687008d442e599e3ddf605aa56
Moritz B <mvb1996@gmail.com>
2014-09-13 05:49:21 +0200

    Updating permission models.
    Beautifying code and adding functionality.

---
+++ application/models/pgroups_permissions_model.php
@@ -0,0 +1,86 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Pgroups_permissions_model extends CI_Model {
+
+	private $_table = 'pgroups_permissions';
+	public function __construct() {
+		parent::__construct();
+		$this->load->database();
+	}
+
+	public function get_permissions_by_group($id) {
+		$query = $this->db->get_where($this->_table, array('pgroup_id' => $id));
+		return $query->result_array();
+	}
+
+	public function get_permission($id) {
+		$query = $this->db->get_where($this->_table, array('permissions_id' => $id));
+		return $query->result_array();
+	}
+
+	public function has_group_permission($groupid, $permissionid) {
+		$query = $this->db->get_where($this->_table, array('pgroup_id' => $groupid, 'permissions_id' => $permissionid));
+		if($query->num_rows() === 1)
+		{
+			$data = $query->row();
+			if($data->value == TRUE)
+			{
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return 0;
+		}
+	}
+
+	public function groupset_have_permissions($groupset, $permissions, &$permissions_array = array()) {
+		foreach($groupset as $g) {
+			$needed = array();
+			if(!isset($permissions_array[$g])) $permissions_array[$g] = array();
+			// Check if permission was already read from db
+			foreach($permissions as $p) {
+				if(isset($permissions_array[$g][$p]) && $permissions_array[$g][$p] == false) return false;
+				if(!isset($permissions_array[$g][$p])) $needed[] = $p;
+				elseif($permissions_array[$g][$p] == true) unset($permissions[$p]);
+			}
+
+			if(count($needed) !== 0) {
+				$this->db->where('pgroup_id', $g);
+				$this->db->where_in('permissions_id', $needed);
+				$result = $this->db->get($this->_table)->result_array();
+				$res = true;
+				foreach($result as $r) {
+					if($r['value'] == false) $res = false;
+					unset($permissions[array_search($r['permissions_id'], $permissions)]);
+					$permissions_array[$g][$r['permissions_id']] = (int) $r['value'];
+				}
+			}
+			foreach($permissions as $p) {
+				$permissions_array[$g][$p] = NULL;
+			}
+			if($res == false) return false;
+			elseif(count($permissions) == 0) return true;
+		}
+		return $permissions;
+	}
+
+
+	public function set_group_permission($groupid, $permissionid, $value) {
+		if($value == true) $value = true;
+		else $value = false;
+		$current_permission = $this->has_group_permission($groupid, $permissionid);
+		if($current_permission===0)
+		{
+			$this->db->insert($this->_table, array(
+				'group_id' => $groupid,
+				'permission_id' => $permissionid,
+				'value' => $value
+			));
+		} elseif($current_permission != $value) {
+			$this->db->where('group_id', $groupid);
+			$this->db->where('permission_id', $permissionid);
+			$this->db->update($this->_table, array('value' => $value));
+		}
+	}
+
+}
