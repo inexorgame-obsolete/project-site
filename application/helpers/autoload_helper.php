928cbb08409b43c541287719a1ca0aaf469997f3
Moritz B <mvb1996@gmail.com>
2014-09-14 06:38:07 +0200

    Changed autoload_helper
    Added function ip()
    Now checks if function already exists helper is loaded and won't
    overwrite it.

--- application/helpers/autoload_helper.php
+++ application/helpers/autoload_helper.php
@@ -1,8 +1,20 @@
 <?php
-function isint(&$val) {
-	if(ctype_digit($val) || is_int($val)) {
-		$val = (int) $val;
-		return true;
+if(!function_exists('isint')) {
+	function isint(&$val) {
+		if(ctype_digit($val) || is_int($val)) {
+			$val = (int) $val;
+			return true;
+		}
+		return false;
 	}
-	return false;
+}
+if(!function_exists('ip')) {
+	function ip() {
+		if($_SERVER['HTTP_X_FORWARDED_FOR']){
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} else { 
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
+		return $ip;
+	}
 }
