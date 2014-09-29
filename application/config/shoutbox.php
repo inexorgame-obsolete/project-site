31f19e9b196cc8bf2e1f6d7ab213820576b920ee
Moritz B <mvb1996@gmail.com>
2014-09-29 19:11:04 +0200

    Clean up
    removed static test-files

--- application/config/shoutbox.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['auto_db_time'] = true;
-
-$config['tables']['shoutbox'] = 'shoutbox';
-$config['tables']['shouts']   = 'shoutbox_shouts';
-$config['tables']['users']    = 'shoutbox_users';
