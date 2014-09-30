59d059c6615c2ac0eb516f9c8d9c0f73142482fa
Moritz B <mvb1996@gmail.com>
2014-09-30 22:29:13 +0200

    Removed private and public recaptcha key

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -1,6 +1,6 @@
 <?php
-$config['keys']['public']  = '6LeSTfoSAAAAADro_vgZQNt1ba7CUAbYRmprJdGS';	// ReCaptcha public key;  Generate one at https://www.google.com/recaptcha/admin#list
-$config['keys']['private'] = '6LeSTfoSAAAAAD-ecDfItizGfJ5p94VcFWcxtDZu';	// ReCaptcha private key; Generate one at https://www.google.com/recaptcha/admin#list
+$config['keys']['public']  = '';	// ReCaptcha public key;  Generate one at https://www.google.com/recaptcha/admin#list
+$config['keys']['private'] = '';	// ReCaptcha private key; Generate one at https://www.google.com/recaptcha/admin#list
 $config['theme']['theme'] = 'clean';
 // $config['theme']['custom_widget'] = '';	// Only needed if theme is custom
 
