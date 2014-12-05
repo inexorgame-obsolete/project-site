ade8c0c387715900d1e2db549db0d21c8bb12376
Moritz B. <mvb1996@gmail.com>
2014-12-05 19:18:26 +0100

    Changed name to inexor

--- application/config/template.php
+++ application/config/template.php
@@ -1,11 +1,11 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['template'] = 'default';
-$config['title']['prefix'] = '';					// String will be added before every title
-$config['title']['suffix'] = ' • Sauerfork';		// String will be added after every title
-$config['title']['default'] = 'Sauerfork';					// Default title if no title is set; The only title not containing the prefix and suffix
+$config['title']['prefix'] = '';		      			// String will be added before every title
+$config['title']['suffix'] = ' • inexor';	    	// String will be added after every title
+$config['title']['default'] = 'inexor';					// Default title if no title is set; The only title not containing the prefix and suffix
 $config['variables'] = array();
-$config['variables']['sitetitle'] = 'Sauerfork';
+$config['variables']['sitetitle'] = 'inexor';
 $config['variables']['base'] = FALSE; 				// Will be overwritten and replaced by base_url();
 $config['variables']['data'] = FALSE;				// Will be overwritten and replaced by base_url() . $data;
 $config['variables']['userdata'] = FALSE;			// Will be overwritten and replaced by base_url() . $userdata;
