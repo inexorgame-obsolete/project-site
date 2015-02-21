801ab911461658738bf3fe183a06a7b0808a848f
Moritz B. <mvb1996@gmail.com>
2015-02-21 01:46:29 +0100

    Added models-config and newline to db-config

---
+++ application/config/models.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Configuration file for the models
+ * Contains which model should use which database config-group (See /application/config/database.php)
+ * And which table.
+ *
+ * If for a model no config is given the default-group will be set and a default table.
+ */
+$config = array();
